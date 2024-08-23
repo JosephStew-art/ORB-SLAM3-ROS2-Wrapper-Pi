37975756a0a49fb502eea3f17cc67294dc7b630f
Joseph Stewart <62858375+JosephStew-art@users.noreply.github.com>
2024-08-23 15:23:12 +0200

    Update mono.cpp
    Added error handling to mono.cpp

--- src/monocular/mono.cpp
+++ src/monocular/mono.cpp
@@ -1,34 +1,55 @@
 #include <iostream>
-#include <algorithm>
-#include <fstream>
-#include <chrono>
+   #include <algorithm>
+   #include <fstream>
+   #include <chrono>
+   #include <thread>
 
-#include "rclcpp/rclcpp.hpp"
-#include "monocular-slam-node.hpp"
+   #include "rclcpp/rclcpp.hpp"
+   #include "monocular-slam-node.hpp"
 
-#include "System.h"
+   #include "System.h"
+   #include "ORBVocabulary.h"
 
+   int main(int argc, char **argv)
+   {
+       if(argc < 3)
+       {
+           std::cerr << "\nUsage: ros2 run orbslam mono path_to_vocabulary path_to_settings" << std::endl;
+           return 1;
+       }
 
-int main(int argc, char **argv)
-{
-    if(argc < 3)
-    {
-        std::cerr << "\nUsage: ros2 run orbslam mono path_to_vocabulary path_to_settings" << std::endl;
-        return 1;
-    }
+       rclcpp::init(argc, argv);
 
-    rclcpp::init(argc, argv);
+       // Load ORB Vocabulary
+       std::cout << "Loading ORB Vocabulary. This could take a while..." << std::endl;
+       ORB_SLAM3::ORBVocabulary* pVocabulary = new ORB_SLAM3::ORBVocabulary();
+       bool bVocLoad = false;
+       try
+       {
+           bVocLoad = pVocabulary->loadFromTextFile(argv[1]);
+           if(!bVocLoad)
+           {
+               std::cerr << "Wrong path to vocabulary. " << std::endl;
+               return 1;
+           }
+       }
+       catch(std::exception& e)
+       {
+           std::cerr << e.what() << std::endl;
+           return 1;
+       }
 
-    // malloc error using new.. try shared ptr
-    // Create SLAM system. It initializes all system threads and gets ready to process frames.
-    bool visualization = true;
-    ORB_SLAM3::System SLAM(argv[1], argv[2], ORB_SLAM3::System::MONOCULAR, visualization);
+       // Create SLAM system. It initializes all system threads and gets ready to process frames.
+       bool visualization = true;
+       ORB_SLAM3::System SLAM(pVocabulary, argv[2], ORB_SLAM3::System::MONOCULAR, visualization);
 
-    auto node = std::make_shared<MonocularSlamNode>(&SLAM);
-    std::cout << "============================ " << std::endl;\
+       auto node = std::make_shared<MonocularSlamNode>(&SLAM);
+       std::cout << "============================ " << std::endl;
 
-    rclcpp::spin(node);
-    rclcpp::shutdown();
+       rclcpp::spin(node);
+       rclcpp::shutdown();
 
-    return 0;
-}
+       delete pVocabulary;
+
+       return 0;
+   }
