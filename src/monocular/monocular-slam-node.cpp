31d57dcd1138974706e24d899542fe3fc299ea61
Joseph <joseph.nomiddlename@gmail.com>
2024-09-18 15:37:11 +0200

    changed some minor things to do with the pi

--- src/monocular/monocular-slam-node.cpp
+++ src/monocular/monocular-slam-node.cpp
@@ -1,4 +1,23 @@
 #include "monocular-slam-node.hpp"
+<<<<<<< HEAD
+#include <chrono>
+
+MonocularSlamNode::MonocularSlamNode(ORB_SLAM3::System* pSLAM)
+    : Node("ORB_SLAM3_ROS2"), m_SLAM(pSLAM), b_continue_session(true)
+{
+    // Initialize webcam
+    m_cap.open(0);
+    if (!m_cap.isOpened()) {
+        RCLCPP_ERROR(this->get_logger(), "Error: Couldn't open the camera.");
+        throw std::runtime_error("Failed to open camera");
+    }
+
+    // Set camera parameters (you may need to adjust these)
+    m_cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    m_cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+    m_cap.set(cv::CAP_PROP_FPS, 30);
+
+=======
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
@@ -12,6 +31,7 @@ MonocularSlamNode::MonocularSlamNode(ORB_SLAM3::System* pSLAM)
         "camera",
         10,
         std::bind(&MonocularSlamNode::GrabImage, this, std::placeholders::_1));
+>>>>>>> 47e7faea95fe71c96f25a95a31caeba224c7be24
     RCLCPP_INFO(this->get_logger(), "MonocularSlamNode initialized");
 }
 
@@ -25,6 +45,32 @@ MonocularSlamNode::~MonocularSlamNode()
     m_SLAM->SavePointCloud("pointcloud.txt");
 }
 
+<<<<<<< HEAD
+void MonocularSlamNode::Run()
+{
+    cv::Mat frame;
+    auto start_time = std::chrono::steady_clock::now();
+
+    while (b_continue_session && rclcpp::ok())
+    {
+        // Capture frame
+        m_cap >> frame;
+        if (frame.empty()) {
+            RCLCPP_ERROR(this->get_logger(), "Error: Blank frame grabbed");
+            break;
+        }
+
+        // Get timestamp
+        auto now = std::chrono::steady_clock::now();
+        auto timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(now - start_time).count() / 1000.0;
+
+        // Pass the image to the SLAM system
+        m_SLAM->TrackMonocular(frame, timestamp);
+
+        // Process any pending callbacks
+        rclcpp::spin_some(this->get_node_base_interface());
+    }
+=======
 void MonocularSlamNode::GrabImage(const sensor_msgs::msg::Image::SharedPtr msg)
 {
     // Copy the ros image message to cv::Mat.
@@ -65,4 +111,5 @@ void MonocularSlamNode::GrabImage(const sensor_msgs::msg::Image::SharedPtr msg)
 
     RCLCPP_INFO(this->get_logger(), "Processing frame");
     m_SLAM->TrackMonocular(image, Utility::StampToSec(msg->header.stamp));
+>>>>>>> 47e7faea95fe71c96f25a95a31caeba224c7be24
 }
--- src/monocular/monocular-slam-node.hpp
+++ src/monocular/monocular-slam-node.hpp
@@ -2,9 +2,13 @@
 #define __MONOCULAR_SLAM_NODE_HPP__
 
 #include "rclcpp/rclcpp.hpp"
+<<<<<<< HEAD
+#include <opencv2/opencv.hpp>
+=======
 #include "sensor_msgs/msg/image.hpp"
 #include <cv_bridge/cv_bridge.h>
 
+>>>>>>> 47e7faea95fe71c96f25a95a31caeba224c7be24
 #include "System.h"
 #include "utility.hpp"
 
@@ -17,6 +21,14 @@ public:
     void Run();  // Make Run() public
 
 private:
+<<<<<<< HEAD
+    ORB_SLAM3::System* m_SLAM;
+    cv::VideoCapture m_cap;
+    bool b_continue_session;
+};
+
+#endif
+=======
     void GrabImage(const sensor_msgs::msg::Image::SharedPtr msg);
 
     ORB_SLAM3::System* m_SLAM;
@@ -25,3 +37,4 @@ private:
 };
 
 #endif // __MONOCULAR_SLAM_NODE_HPP__
+>>>>>>> 47e7faea95fe71c96f25a95a31caeba224c7be24
